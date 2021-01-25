507db10a65f70c9cd26a247247497d34bbce1718
cobyfoster <cobyfoster99@gmail.com>
2021-01-25 16:27:40 +0000

    linked orders to admin

--- checkout/signals.py
+++ checkout/signals.py
@@ -10,5 +10,5 @@ def update_on_save(sender, instance, created, **kwargs):
 
 
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_save(sender, instance, **kwargs):
+def update_on_delete(sender, instance, **kwargs):
     instance.order.update_total()
