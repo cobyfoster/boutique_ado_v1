4b9fbaec66d4f94bac58de9c3a481a0e59aa9e38
cobyfoster <cobyfoster99@gmail.com>
2021-01-27 11:31:16 +0000

    Add edit and delete products

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,5 @@
-from django import forms 
+
+from django import forms
 from .models import Product, Category
 
 
@@ -8,10 +9,10 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
-    def __init__(self, *args, *kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
-        friendly_nmes = [(c.id, c.get_friendly_name()) for c in categories]
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
